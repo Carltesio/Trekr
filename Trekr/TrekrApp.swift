bc089d2ab1c41f3df7b672b953230c25c31fddc5
carlos <carlos.delgado@hotmail.it>
2021-05-06 13:00:05 +0200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Carlos Delgado on 2021-05-06.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
