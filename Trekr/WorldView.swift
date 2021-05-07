309173d5a1dd6ee22f690d384d4b52e01cc5a5eb
carlos <carlos.delgado@hotmail.it>
2021-05-07 16:45:24 +0200

    Adding Map, observable objects like location to be used all over the App as a global state

---
+++ Trekr/WorldView.swift
@@ -0,0 +1,41 @@
+//
+//  WorldView.swift
+//  Trekr
+//
+//  Created by Carlos Delgado on 2021-05-07.
+//
+
+import MapKit
+import SwiftUI
+
+struct WorldView: View {
+    @EnvironmentObject var locations: Locations
+   @State var region = MKCoordinateRegion(
+        center:
+            CLLocationCoordinate2D(latitude: 44.4056, longitude: 8.9463),
+        span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40)
+        )
+    var body: some View {
+       Map(coordinateRegion: $region,
+           annotationItems: locations.places) {
+            location in
+            MapAnnotation(coordinate:
+                CLLocationCoordinate2D(latitude:
+                location.latitude, longitude:
+                location.longitude)) {
+                Image(location.country)
+                    .resizable()
+                    .cornerRadius(10)
+                    .frame(width: 40, height: 20)
+                    .shadow(radius: 3)
+          }
+       }
+        .navigationTitle("Locations")
+    }
+}
+
+struct WorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldView()
+    }
+}
